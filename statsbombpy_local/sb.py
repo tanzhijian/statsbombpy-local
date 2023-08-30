107a9e50e2d56e20cedd56661bfd83fede149e4d
tanzhijian <i@tanzhijian.org>
2023-08-30 18:47:36 +0800

    matches, lineups and events

--- statsbombpy_local/sb.py
+++ statsbombpy_local/sb.py
@@ -8,17 +8,88 @@ from ._config import BASE_PATH, BASE_URL, Paths
 from ._utils import read_data
 
 
+_local_paths = Paths(BASE_PATH)
+_remote_paths = Paths(BASE_URL)
+
+
 def competitions(
     fmt: str = "dataframe",
     creds: dict = DEFAULT_CREDS,
-    local_paths: Paths = Paths(BASE_PATH),
-    remote_paths: Paths = Paths(BASE_URL),
+    local_paths: Paths = _local_paths,
 ) -> DataFrame | dict:
     if has_auth(creds) is True:
         return sb.competitions(fmt, creds)
 
     with Mocker() as m:
         data = read_data(local_paths.competitions())
-        m.get(remote_paths.competitions(), json=data)
-        competitions = sb.competitions()
-    return competitions
+        m.get(_remote_paths.competitions(), json=data)
+        sb_competitions = sb.competitions(fmt)
+    return sb_competitions
+
+
+def matches(
+    competition_id: int,
+    season_id: int,
+    fmt="dataframe",
+    creds: dict = DEFAULT_CREDS,
+    local_paths: Paths = _local_paths,
+) -> DataFrame | dict:
+    if has_auth(creds) is True:
+        return sb.matches(competition_id, season_id, fmt, creds)
+
+    with Mocker() as m:
+        data = read_data(local_paths.matches(competition_id, season_id))
+        m.get(_remote_paths.matches(competition_id, season_id), json=data)
+        sb_matches = sb.matches(competition_id, season_id, fmt)
+    return sb_matches
+
+
+def lineups(
+    match_id: int,
+    fmt="dataframe",
+    creds: dict = DEFAULT_CREDS,
+    local_paths: Paths = _local_paths,
+) -> dict[str, DataFrame]:
+    if has_auth(creds) is True:
+        return sb.lineups(match_id, fmt, creds)
+
+    with Mocker() as m:
+        data = read_data(local_paths.lineups(match_id))
+        m.get(_remote_paths.lineups(match_id), json=data)
+        sb_lineups = sb.lineups(match_id, fmt)
+    return sb_lineups
+
+
+def events(
+    match_id: int,
+    split: bool = False,
+    filters: dict = {},
+    fmt: str = "dataframe",
+    flatten_attrs: bool = True,
+    creds: dict = DEFAULT_CREDS,
+    include_360_metrics=False,
+    local_paths: Paths = _local_paths,
+) -> DataFrame | dict:
+    if has_auth(creds) is True:
+        return sb.events(
+            match_id,
+            split,
+            filters,
+            fmt,
+            flatten_attrs,
+            creds,
+            include_360_metrics,
+        )
+
+    with Mocker() as m:
+        data = read_data(local_paths.events(match_id))
+        m.get(_remote_paths.events(match_id), json=data)
+        sb_events = sb.events(
+            match_id=match_id,
+            split=split,
+            filters=filters,
+            fmt=fmt,
+            flatten_attrs=flatten_attrs,
+            include_360_metrics=include_360_metrics,
+        )
+    return sb_events
--- tests/test_sb.py
+++ tests/test_sb.py
@@ -5,7 +5,7 @@ import pytest
 from pandas import DataFrame
 from statsbombpy.api_client import NoAuthWarning
 
-from statsbombpy_local.sb import competitions
+from statsbombpy_local.sb import competitions, matches, lineups, events
 from statsbombpy_local._config import Paths
 
 
@@ -14,8 +14,27 @@ def ignore_no_auth_warning():
     warnings.filterwarnings("ignore", category=NoAuthWarning)
 
 
+path = Path(Path.cwd(), "tests/data")
+
+
 def test_competitions() -> None:
-    path = Path(Path.cwd(), "tests/data/")
     c = competitions(local_paths=Paths(path))
     if isinstance(c, DataFrame):
         assert c.loc[0].competition_id == 9
+
+
+def test_matches() -> None:
+    m = matches(2, 44, local_paths=Paths(path))
+    if isinstance(m, DataFrame):
+        assert m.loc[0].away_team == "Arsenal"
+
+
+def test_lineups() -> None:
+    lu = lineups(7298, local_paths=Paths(path))
+    assert lu["Chelsea FCW"].loc[0].jersey_number == 16
+
+
+def test_events() -> None:
+    e = events(7298, local_paths=Paths(path))
+    if isinstance(e, DataFrame):
+        assert e.loc[0].possession_team == "Manchester City WFC"
